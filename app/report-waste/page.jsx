b88bc07be4968e9cea1b48bcddb8c749ff0d4e9c
Ryanmrgo <pyaesone150945@gmail.com>
2025-08-20 02:24:16 +0630

    report waste add

---
+++ app/report-waste/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ReportWaste = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-10">
+      <h1 className="text-3xl font-bold mb-6">Report Waste</h1>
+      <p className="mb-4 text-gray-700">Use this page to report waste in your area. (Map and form coming soon!)</p>
+      {/* Map and form will be added here */}
+    </div>
+  );
+};
+
+export default ReportWaste;
