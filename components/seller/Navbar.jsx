731f4511fad9b1b05b93e8c181bbe145ebd51954
Ryanmrgo <pyaesone150945@gmail.com>
2025-10-13 01:44:23 +0630

    seller log out button fix

--- components/seller/Navbar.jsx
+++ components/seller/Navbar.jsx
@@ -1,16 +1,31 @@
+
+"use client";
 import React from 'react'
 import { assets } from '../../assets/assets'
 import Image from 'next/image'
 import { useAppContext } from '@/context/AppContext'
+import { useClerk } from '@clerk/nextjs';
 
 const Navbar = () => {
 
-  const { router } = useAppContext()
+
+  const { router } = useAppContext();
+  const { signOut } = useClerk();
+
+  const handleLogout = async () => {
+    await signOut();
+    router.push('/');
+  };
 
   return (
     <div className='flex items-center px-4 md:px-8 py-3 justify-between border-b'>
   <Image onClick={()=>router.push('/')} className='w-28 lg:w-32 cursor-pointer' src={assets.logo} alt="" />
-      <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
+      <button
+        className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'
+        onClick={handleLogout}
+      >
+        Logout
+      </button>
     </div>
   )
 }
